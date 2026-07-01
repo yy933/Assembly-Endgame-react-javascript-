974264cb263750a2763c4928a025059c633895d3
yy933 <116449560+yy933@users.noreply.github.com>
2026-07-01 19:43:25 +0800

    feat: add word display section and its styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import languages from "./data/languages";
 
 export default function App() {
+  const [currentWord, setCurrentWord] = React.useState("react");
+  const wordElements = currentWord.split("").map((letter, index) => {
+    return (
+      <span key={index} className="letter">
+        {letter.toUpperCase()}
+      </span>
+    );
+  })
   // Create a new array of JSX elements from the languages array
   const languageElements = languages.map((language) => {
     const styles = {
@@ -14,6 +22,19 @@ export default function App() {
       </span>
     );
   });
+
+  /**
+   * Goal: Build out the main parts of our app
+   *
+   * Challenge:
+   * 1. Save a "currentWord" in state. Initialize as "react".
+   * 2. Map over the letters of the word (you'll need to turn
+   *    the string into an array of letters first) and display
+   *    each one as a <span>. Capitalize the letters when
+   *    displaying them.
+   * 3. Style to look like the design. You can get the underline
+   *    effect on the box using `border-bottom`.
+   */
 
   return (
     <main>
@@ -29,6 +50,7 @@ export default function App() {
         <p>Well done! 🎉</p>
       </section>
       <section className="language-chips">{languageElements}</section>
+      <section className="word">{wordElements}</section>
     </main>
   );
 }
